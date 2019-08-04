6e54d1c22c838dcc3b68e66dd171ea82708009b0
Trish Ogen <patriciaogen@gmail.com>
2019-08-04 23:33:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,7 +19,7 @@ class App < Sinatra::Base
   get '/say/:word1/:word2/:word3/:word4/:word5' do
     phrases = [params[:word1], params[:word2], params[:word3], params[:word4],
                 params[:word5]]
-                
+
     @full_phrase = phrases.join(" ")
     "#{@full_phrase}."
   end
